1b7dd91ce12a77777767a7af9e9c4bc16b5c2032
Daud L Namayala <daudnamayala@gmail.com>
2023-07-06 00:41:27 +0300

    Shipping static files

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -25,8 +25,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
