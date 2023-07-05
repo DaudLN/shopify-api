11cf5f7cca5512f8204ad5bc00f6fa486836c494
Daud L Namayala <daudnamayala@gmail.com>
2023-07-06 01:02:43 +0300

    Shipping static files

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -94,7 +94,7 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "staticfile"
 
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_ROOT = BASE_DIR / "media"
 
