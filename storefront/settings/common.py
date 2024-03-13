f526dd0589cea86deba3bd118359f3a9a9c15d03
daudln <daudnamayala@gmail.com>
2024-03-13 21:33:51 +0300

    Mail conf

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -203,21 +203,6 @@ LOGGING = {
 
 
 
-DEFAULT_FROM_EMAIL = "admin.store@storefront.com"
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.gmail.com"
-
-EMAIL_HOST_USER = "daudnamayala@gmail.com"
-
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-EMAIL_PORT = 587
-
-EMAIL_USE_TLS = True
-
-
 SPECTACULAR_SETTINGS = {
     "TITLE": "Shopify API",
     "DESCRIPTION": "Explore the features and endpoints supported by our app",
